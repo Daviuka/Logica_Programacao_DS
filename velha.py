520a57f74d777aec5407dbb95e4ba32110c5dba0
Daviuka <davi.cardoso@sesidf.org.br>
2024-08-20 10:28:35 -0300

    Davi

--- velha.py
+++ velha.py
@@ -15,6 +15,7 @@ velha = [
 
 def tela():
     global velha
+    global jogadas
     os.system("cls")
     print("    0   1   2")
     print("0:  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
@@ -22,5 +23,7 @@ def tela():
     print("1:  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2])
     print("   -----------")
     print("2:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
-    print("Jogadas: " )
+    print("Jogadas: " + Fore.GREEN + str(jogadas) + Fore.RESET)
+
+tela()
 
